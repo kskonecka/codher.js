11bcb1a7afd27a0b789ec9da5b13745b4633c856
Emilis Sapronas <esapronas@hotmail.com>
2016-02-19 09:12:44 +0100

    Var keyword is for variables, rename all functions to function declaration. Functions are also camelCase

--- exercises/examples/03_array.js
+++ exercises/examples/03_array.js
@@ -13,13 +13,13 @@ colors.push("Purple");
 console.log(colors);
 
 // Let's remove a specific value
-var remove_from_array = function(needle, haystack) {
+function removeFromArray(needle, haystack) {
   var index = haystack.indexOf(needle);
   if(index > -1) {
     haystack.splice(index, 1);
   }
-};
+}
 
 // Wait, potato is not a color
-remove_from_array("Potato", colors);
+removeFromArray("Potato", colors);
 console.log(colors);
